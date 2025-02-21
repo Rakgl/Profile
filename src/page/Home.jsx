e8d8d23ff8d64d07a539b745c6eef0badacd79cf
Rak <kosal.linnorak@camsolution.biz>
2025-02-21 14:22:48 +0700

    new update

---
+++ src/page/Home.jsx
@@ -0,0 +1,13 @@
+import LoadingWord from "../components/Home/LoadingWord";
+
+const Home = () => {
+  return (
+    <>
+        <div className="mt-5">
+            <LoadingWord />
+        </div>
+    </>
+  );
+};
+
+export default Home;
